d377d7b397552501504b64b15794afcfefc46a67
Shrey <macbook@Shrey.local>
2021-04-26 00:11:37 -0400

    Final Changes and TDD added in JEST

--- app.js
+++ app.js
@@ -1,35 +1,37 @@
+//Author - Shrey Patel
 var express = require("express");
 var app = express();
 var request = require("request");
 app.set("view engine", "ejs");
+
+// *********  List of URLs to get data *********
 const baseUrl = "https://vpic.nhtsa.dot.gov/api/vehicles/decodevinvalues/";
 const endUrl = "?format=json";
 const allMakeUrl = "https://vpic.nhtsa.dot.gov/api/vehicles/getallmakes?format=json";
 const allManufacturerUrl = "https://vpic.nhtsa.dot.gov/api/vehicles/getallmanufacturers?ManufacturerType=Intermediate&format=json";
+//***********************************************/
 app.use(express.static(__dirname + '/public'));
 
+// ROUTE PART  , form will be display to enter vin number
 app.get("/", function(req, res) {
 
     res.render("index");
 });
 
-
+//This route part will display the output based on user input
 app.get("/results", function(req, res) {
     var query = req.query.vin;
     var url = baseUrl + query + endUrl;
     console.log(url);
     request(url, function(error, response, body) {
-
         if (!error && response.statusCode == 200) {
             var results = JSON.parse(body)
             res.render("results", data = results);
-
-
-
         }
     });
 });
 
+//This route will display ALL MAKE
 app.get("/make", function(req, res) {
     request(allMakeUrl, function(error, response, body) {
         if (!error && response.statusCode == 200) {
@@ -39,6 +41,7 @@ app.get("/make", function(req, res) {
     });
 });
 
+//This route will display ALL Manufacturer
 app.get("/manufacturer", function(req, res) {
     request(allManufacturerUrl, function(error, response, body) {
         if (!error && response.statusCode == 200) {
@@ -47,6 +50,8 @@ app.get("/manufacturer", function(req, res) {
         }
     });
 });
+
+
 app.listen(process.env.PORT || 3000, function() {
     console.log("NHTSA API is running......!!!", this.address().port, app.settings.env);
 });
---
+++ app.test.js
@@ -0,0 +1,14 @@
+
+
+
+describe('First group of tests', () => {
+    it('Test Driven Development is working', () => {
+       expect(1).toBe(1);
+    });
+ });
+
+ describe('Second Test', () => {
+    it('Check the VIN number', () => {
+       
+    });
+ });
